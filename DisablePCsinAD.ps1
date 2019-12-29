fe1af1bb395cea6f8fa6dadb3491b5fb222bad01
zachchism <48837572+zachchism@users.noreply.github.com>
2019-12-29 15:50:09 -0500

    Add files via upload

---
+++ DisablePCsinAD.ps1
@@ -0,0 +1,13 @@
+﻿$Computers = Get-Content c:\temp\computers.txt
+
+foreach ($Computer in $Computers) {
+    $ADComputer = $null
+    $ADComputer = Get-ADComputer $Computer -Properties Description
+
+    if ($ADComputer) {
+        Add-Content C:\temp\computers.log -Value "Found $Computer, disabling"
+        Set-ADComputer $ADComputer -Description "Computer Disabled on $(Get-Date)" -Enabled $false
+    } else {
+        Add-Content C:\temp\computers.log -Value "$Computer not in Active Directory"
+    }
+}
