e2d920b52114c3e87e9067552063aebc8f7d3f2e
zachchism <48837572+zachchism@users.noreply.github.com>
2022-06-30 20:52:40 -0400

    Update DHCP_Report.ps1

--- Reporting/DHCP_Report.ps1
+++ Reporting/DHCP_Report.ps1
@@ -1,1 +1,21 @@
+$ServerList = "SERVERLIST"
+$date = Get-Date -format "yyyy-MM-dd"
+$FileName = "C:\temp\" + $date + "_MonthlyReport" + ".csv"
 
+foreach ($Server in $ServerList){
+$ScopePull = Get-DhcpServerv4Scope -ComputerName $Server | Select ScopeID,Name
+
+    foreach ($DHCPScope in $ScopePull){
+
+    $Tempvar = Get-DhcpServerv4OptionValue -ComputerName $Server -ScopeId $DHCPScope.ScopeId -all | select @{Name=’Name’;Expression={[string]::join(“;”, ($_.Name))}}, @{Name=’Value’;Expression={[string]::join(“;”, ($_.Value))}}
+    $TempvarObject = [PSCustomObject]$TempVar
+
+    
+    $TempVarObject | Add-Member -MemberType NoteProperty -Name 'ScopeName' -Value $DHCPScope.Name -Force
+    $TempVarObject | Add-Member -MemberType NoteProperty -Name 'ScopeID' -Value $DHCPScope.ScopeID -Force
+    $TempVarObject | Add-Member -MemberType NoteProperty -Name 'ServerName' -Value $Server -Force
+
+
+    $TempVarObject | Export-csv -Path C:\temp\export.csv -NoTypeInformation -Append
+                                        }
+                              }
