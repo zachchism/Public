c2171304b2ec1726bcb10a895b87c8bcfb776d27
zachchism <48837572+zachchism@users.noreply.github.com>
2022-10-15 15:08:17 -0400

    Update TaskSchedule_Audit.ps1

--- Reporting/TaskSchedule_Audit.ps1
+++ Reporting/TaskSchedule_Audit.ps1
@@ -4,6 +4,8 @@ $ExportList=@()
 Foreach ($task in $TaskList) {
 $TaskDetails = Get-ScheduledTaskInfo -InputObject $Task
 write-output $Task | Format-Table -Property TaskName, Author, Description
+
+#Assign var
 $TaskAuthor = $task.Author
 $TaskTitle = $Task.TaskName
 $TaskDescription = $Task.Description
@@ -12,9 +14,11 @@ $TaskLastResult = $TaskDetails.LastTaskResult
 $TaskNextRun = $TaskDetails.NextRunTime
 $TaskMissed = $TaskDetails.NumberOfMissedRuns
 
+#Object creation
 $Export = New-Object psObject -Property @{'TaskAuthor'=$TaskAuthor; 'TaskTitle' = $TaskTitle; 'TaskDescription' = $TaskDescription; 'LastRunTime' = $TaskLastRun;`
 'LastRunResult' = $TaskLastResult; 'TaskNextRun' = $TaskNextRun; 'MissedRuns' = $TaskMissed}
 
+#Export member add from properties
 $Export | Add-Member -MemberType NoteProperty -name "TaskAuthor" -Value $TaskAuthor -Force
 $Export | Add-member -MemberType NoteProperty -name "TaskTitle" -Value $TaskTitle -Force
 $Export | Add-Member -MemberType NoteProperty -Name "TaskDescription" -Value $TaskDescription -force
@@ -24,7 +28,7 @@ $Export | Add-member -MemberType NoteProperty -Name "TaskNextRun" -Value $TaskNe
 $Export | Add-Member -MemberType NoteProperty -Name "MissedRuns" -Value $TaskMissed -Force
 
 
-
+#Add members to final ExportList
 $ExportList += $Export
 }
 
